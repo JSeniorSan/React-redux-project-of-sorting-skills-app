5ccb49053f84871f53903589c2a018a91199093f
Tekai_ <vip007_007@mail.ru>
2023-08-09 02:05:58 +1000

    Start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./index.css";
+function App() {
+  return (
+    <div className="flex-col">
+      <div className="border w-full h-56  ">
+        <Header />
+      </div>
+      <div>
+        <div>
+          <div>hi</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
