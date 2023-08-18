73edfb1f28b7ba75fef7c10d66928abf2f64451b
Tekai_ <vip007_007@mail.ru>
2023-08-19 00:25:20 +1000

    Added redux-persist

--- src/root.jsx
+++ src/root.jsx
@@ -2,11 +2,15 @@ import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./store/index";
 import App from "./App";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/index";
 function RootProviders() {
   return (
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,30 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./root-reducers";
+import { logger } from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Middleware
+const middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+//PersistConfig
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["positions", "filter"],
+  // blacklist: [],
+};
+//PersistReducer
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 export { store };
+export const persistor = persistStore(store);
